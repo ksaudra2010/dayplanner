cbc8071c3fe0c1fa3908351cfef39378b5ed9eb3
Code Minion <esugrad2004@gmail.com>
2020-01-11 21:55:31 -0600

    edited html formatting

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dayplanner
 Calendar Day Planner
+USE
+Add meetings and reminders to this calendar to make your day more efficient. 
+Click Save once you've added your task. 
+Calendar items in the past will be gray, current hour is red, and future hours are green. 
+
--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDay = moment().format('MMMM Do YYYY, h:mm a');
 $('#currentDay').text(currentDay)
+setInterval(function () { refresh; }, 1000);
 
 var currentHour = new Date().getHours()
 console.log(currentHour);
-$('.description').each(function(){
+$('.description').each(function () {
     var time = $(this).data('time');
-    if (currentHour>time) {
+    if (currentHour > time) {
         $(this).addClass('past')
-    } else if (currentHour<time) {
+    } else if (currentHour < time) {
         $(this).addClass('future')
     }
-    else if (currentHour==time) {
+    else if (currentHour == time) {
         $(this).addClass('present')
     }
     var savedDescription = localStorage.getItem(time)
-    if (savedDescription){
+    if (savedDescription) {
         $(this).val(savedDescription)
     }
-    setInterval(function(){ refresh; }, 1000);
+
 })
-$('.saveBtn').on('click', function() {
+$('.saveBtn').on('click', function () {
     var description = $(this).parent().parent().find('.description')
     var time = description.data('time');
-    localStorage.setItem(time,description.val())
+    localStorage.setItem(time, description.val())
 })
